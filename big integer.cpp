90150608b0ec350d55742cc183c67e4f27efa65e
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-01-09 13:56:35 -0800

    Create big integer.cpp

---
+++ big integer.cpp
@@ -0,0 +1,516 @@
+/* BIG INTEGER IN C++    */
+
+/*    
+
+   #include <bits/stdc++.h>
+
+using namespace std;
+
+typedef int64_t ll;
+typedef long long ll;
+
+#define EL printf("\n")
+#define pb push_back
+#define FOR(i,l,r) for (int i=l;i<=r;i++)
+#define FORD(i,r,l) for (int i=r;i>=l;i--)
+
+const int base = 1e9;
+typedef vector<int> BigInt;
+
+void Set(BigInt &a) {
+    while (a.size() > 1 && a.back() == 0) a.pop_back();
+}
+
+void Print(BigInt a) {
+    Set(a);
+    printf("%d", (a.size() == 0) ? 0 : a.back());
+    FORD(i,a.size()-2,0) printf("%09d", a[i]); EL;
+}
+
+BigInt Integer(string s) {
+    BigInt ans;
+    if (s[0] == '-') return ans;
+    if (s.size() == 0) {ans.pb(0); return ans;}
+    while (s.size()%9 != 0) s = '0'+s;
+    for (int i=0;i<s.size();i+=9) {
+        int v = 0;
+        for (int j=i;j<i+9;j++) v = v*10+(s[j]-'0');
+        ans.insert(ans.begin(),v);
+    }
+    Set(ans);
+    return ans;
+}
+
+BigInt Integer(char c[]) {
+    string s = "";
+    FOR(i,0,strlen(c)-1) s = s + c[i];
+    return Integer(s);
+}
+
+BigInt Integer(ll x) {
+    string s = "";
+    while (x > 0) s = char(x%10+'0') + s, x /= 10;
+    return Integer(s);
+}
+
+BigInt Integer(int x) {
+    return Integer((ll) x);
+}
+
+void operator >> (istream &in, BigInt &a) {
+    string s;
+    getline(cin, s);
+    a = Integer(s);
+}
+
+void operator << (ostream &out, BigInt a) {
+    Print(a);
+}
+
+bool operator < (BigInt a, BigInt b) {
+    Set(a);
+    Set(b);
+    if (a.size() != b.size()) return (a.size() < b.size());
+    FORD(i,a.size()-1,0)
+        if (a[i] != b[i]) return (a[i] < b[i]);
+    return false;
+}
+
+bool operator > (BigInt a, BigInt b) {
+    return (b < a);
+}
+
+bool operator == (BigInt a, BigInt b) {
+    return (!(a < b) && !(b < a));
+}
+
+bool operator <= (BigInt a, BigInt b) {
+    return (a < b || a == b);
+}
+
+bool operator >= (BigInt a, BigInt b) {
+    return (b < a || b == a);
+}
+
+bool operator < (BigInt a, int b) {
+    return (a < Integer(b));
+}
+
+bool operator > (BigInt a, int b) {
+    return (a > Integer(b));
+}
+
+bool operator == (BigInt a, int b) {
+    return (a == Integer(b));
+}
+
+bool operator >= (BigInt a, int b) {
+    return (a >= Integer(b));
+}
+
+bool operator <= (BigInt a, int b) {
+    return (a <= Integer(b));
+}
+
+BigInt max(BigInt a, BigInt b) {
+    if (a > b) return a;
+    return b;
+}
+
+BigInt min(BigInt a, BigInt b) {
+    if (a < b) return a;
+    return b;
+}
+
+BigInt operator + (BigInt a, BigInt b) {
+    Set(a);
+    Set(b);
+    BigInt ans;
+    int carry = 0;
+    FOR(i,0,max(a.size(), b.size())-1) {
+        if (i < a.size()) carry += a[i];
+        if (i < b.size()) carry += b[i];
+        ans.pb(carry%base);
+        carry /= base;
+    }
+    if (carry) ans.pb(carry);
+    Set(ans);
+    return ans;
+}
+
+BigInt operator + (BigInt a, int b) {
+    return a + Integer(b);
+}
+
+BigInt operator ++ (BigInt &a) { // ++a
+    a = a + 1;
+    return a;
+}
+
+void operator += (BigInt &a, BigInt b) {
+    a = a + b;
+}
+
+void operator += (BigInt &a, int b) {
+    a = a + b;
+}
+
+BigInt operator &mdash; (BigInt a, BigInt b) {
+    Set(a);
+    Set(b);
+    BigInt ans;
+    int carry = 0;
+    FOR(i,0,a.size()-1) {
+        carry += a[i] &mdash; (i < b.size() ? b[i] : 0);
+        if (carry < 0) ans.pb(carry+base), carry = -1;
+        else ans.pb(carry), carry = 0;
+    }
+    Set(ans);
+    return ans;
+}
+
+BigInt operator &mdash; (BigInt a, int b) {
+    return a &mdash; Integer(b);
+}
+
+void operator -- (BigInt &a) { // --a
+    a = a &mdash; 1;
+}
+
+void operator -= (BigInt &a, BigInt b) {
+    a = a + b;
+}
+
+void operator -= (BigInt &a, int b) {
+    a = a &mdash; b;
+}
+
+BigInt operator * (BigInt a, BigInt b) {
+    Set(a);
+    Set(b);
+    BigInt ans;
+    ans.assign(a.size()+b.size(), 0);
+    FOR(i,0,a.size()-1) {
+        ll carry = 0ll;
+        for (int j=0;j<b.size() || carry > 0;j++) {
+            ll s = ans[i+j] + carry + (ll)a[i]*(j<b.size()?(ll)b[j]:0ll);
+            ans[i+j] = s%base;
+            carry = s/base;
+        }
+    }
+    Set(ans);
+    return ans;
+}
+
+BigInt operator * (BigInt a, int b) {
+    return a * Integer(b);
+}
+
+void operator *= (BigInt &a, BigInt b) {
+    a = a * b;
+}
+
+void operator *= (BigInt &a, int b) {
+    a = a * b;
+}
+
+
+
+BigInt operator / (BigInt a, BigInt b) {
+    Set(a);
+    Set(b);
+    if (b == Integer(0)) return Integer("-1");
+    BigInt ans, cur;
+    FORD(i,a.size()-1,0) {
+        cur.insert(cur.begin(), a[i]);
+        int x = 0, L = 0, R = base;
+        while (L <= R) {
+            int mid = (L+R)>>1;
+            if (b*Integer(mid) > cur) {
+                x = mid;
+                R = mid-1;
+            }
+            else
+                L = mid+1;
+        }
+        cur = cur &mdash; Integer(x-1)*b;
+        ans.insert(ans.begin(),x-1);
+    }
+    Set(ans);
+    return ans;
+}
+
+BigInt operator / (BigInt a, int b) {
+    Set(a);
+    BigInt ans;
+    ll cur = 0ll;
+    FORD(i,a.size()-1,0) {
+        cur = (cur*(ll)base + (ll)a[i]);
+        ans.insert(ans.begin(),cur/b);
+        cur %= b;
+    }
+    Set(ans);
+    return ans;
+}
+
+void operator /= (BigInt &a, BigInt b) {
+    a = a / b;
+}
+
+void operator /= (BigInt &a, int b) {
+    a = a / b;
+}
+
+BigInt operator % (BigInt a, BigInt b) {
+    Set(a);
+    Set(b);
+    if (b == Integer(0)) return Integer("-1");
+    BigInt ans;
+    FORD(i,a.size()-1,0) {
+        ans.insert(ans.begin(), a[i]);
+        int x = 0, L = 0, R = base;
+        while (L <= R) {
+            int mid = (L+R)>>1;
+            if (b*Integer(mid) > ans) {
+                x = mid;
+                R = mid-1;
+            }
+            else
+                L = mid+1;
+        }
+        ans = ans &mdash; Integer(x-1)*b;
+    }
+    Set(ans);
+    return ans;
+}
+
+int operator % (BigInt a, int b) {
+    Set(a);
+    if (b == 0) return -1;
+    int ans = 0;
+    FORD(i,a.size()-1,0)
+        ans = (ans*(base%b) + a[i]%b)%b;
+    return ans;
+}
+
+void operator %= (BigInt &a, BigInt b) {
+    a = a % b;
+}
+
+void operator %= (BigInt &a, int b) {
+    a = a % Integer(b);
+}
+
+BigInt gcd(BigInt a, BigInt b) {
+    Set(a);
+    Set(b);
+    while (b > Integer(0)) {
+        BigInt r = a%b;
+        a = b;
+        b = r;
+    }
+    Set(a);
+    return a;
+}
+
+BigInt lcm(BigInt a, BigInt b) {
+    return (a*b/gcd(a,b));
+}
+
+
+BigInt sqrt(BigInt a) {
+    BigInt x0 = a, x1 = (a+1)/2;
+    while (x1 < x0) {
+        x0 = x1;
+        x1 = (x1+a/x1)/2;
+    }
+    return x0;
+}
+
+BigInt pow(BigInt a, BigInt b) {
+    if (b == Integer(0)) return Integer(1);
+    BigInt tmp = pow(a, b/2);
+    if (b%2 == 0) return tmp * tmp;
+    return tmp * tmp * a;
+}
+
+BigInt pow(BigInt a, int b) {
+    return pow(a,(Integer(b)));
+}
+
+int log(int n, BigInt a) { // log_n(a)
+    Set(a);
+    int ans = 0;
+    while (a > Integer(1)) {
+        ans++;
+        a /= n;
+    }
+    return ans;
+}
+
+
+
+
+
+      */
+
+
+
+template <typename T>
+T inverse(T a, T m) {
+  T u = 0, v = 1;
+  while (a != 0) {
+    T t = m / a;
+    m -= t * a; swap(a, m);
+    u -= t * v; swap(u, v);
+  }
+  assert(m == 1);
+  return u;
+}
+ 
+template <typename T>
+class Modular {
+ public:
+  using Type = typename decay<decltype(T::value)>::type;
+ 
+  constexpr Modular() : value() {}
+  template <typename U>
+  Modular(const U& x) {
+    value = normalize(x);
+  }
+ 
+  template <typename U>
+  static Type normalize(const U& x) {
+    Type v;
+    if (-mod() <= x && x < mod()) v = static_cast<Type>(x);
+    else v = static_cast<Type>(x % mod());
+    if (v < 0) v += mod();
+    return v;
+  }
+ 
+  const Type& operator()() const { return value; }
+  template <typename U>
+  explicit operator U() const { return static_cast<U>(value); }
+  constexpr static Type mod() { return T::value; }
+ 
+  Modular& operator+=(const Modular& other) { if ((value += other.value) >= mod()) value -= mod(); return *this; }
+  Modular& operator-=(const Modular& other) { if ((value -= other.value) < 0) value += mod(); return *this; }
+  template <typename U> Modular& operator+=(const U& other) { return *this += Modular(other); }
+  template <typename U> Modular& operator-=(const U& other) { return *this -= Modular(other); }
+  Modular& operator++() { return *this += 1; }
+  Modular& operator--() { return *this -= 1; }
+  Modular operator++(int) { Modular result(*this); *this += 1; return result; }
+  Modular operator--(int) { Modular result(*this); *this -= 1; return result; }
+  Modular operator-() const { return Modular(-value); }
+ 
+  template <typename U = T>
+  typename enable_if<is_same<typename Modular<U>::Type, int>::value, Modular>::type& operator*=(const Modular& rhs) {
+#ifdef _WIN32
+    uint64_t x = static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value);
+    uint32_t xh = static_cast<uint32_t>(x >> 32), xl = static_cast<uint32_t>(x), d, m;
+    asm(
+      "divl %4; \n\t"
+      : "=a" (d), "=d" (m)
+      : "d" (xh), "a" (xl), "r" (mod())
+    );
+    value = m;
+#else
+    value = normalize(static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value));
+#endif
+    return *this;
+  }
+  template <typename U = T>
+  typename enable_if<is_same<typename Modular<U>::Type, long long>::value, Modular>::type& operator*=(const Modular& rhs) {
+    long long q = static_cast<long long>(static_cast<long double>(value) * rhs.value / mod());
+    value = normalize(value * rhs.value - q * mod());
+    return *this;
+  }
+  template <typename U = T>
+  typename enable_if<!is_integral<typename Modular<U>::Type>::value, Modular>::type& operator*=(const Modular& rhs) {
+    value = normalize(value * rhs.value);
+    return *this;
+  }
+ 
+  Modular& operator/=(const Modular& other) { return *this *= Modular(inverse(other.value, mod())); }
+ 
+  friend const Type& abs(const Modular& x) { return x.value; }
+ 
+  template <typename U>
+  friend bool operator==(const Modular<U>& lhs, const Modular<U>& rhs);
+ 
+  template <typename U>
+  friend bool operator<(const Modular<U>& lhs, const Modular<U>& rhs);
+ 
+  template <typename V, typename U>
+  friend V& operator>>(V& stream, Modular<U>& number);
+ 
+ private:
+  Type value;
+};
+ 
+template <typename T> bool operator==(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value == rhs.value; }
+template <typename T, typename U> bool operator==(const Modular<T>& lhs, U rhs) { return lhs == Modular<T>(rhs); }
+template <typename T, typename U> bool operator==(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) == rhs; }
+ 
+template <typename T> bool operator!=(const Modular<T>& lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(const Modular<T>& lhs, U rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(U lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+ 
+template <typename T> bool operator<(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value < rhs.value; }
+ 
+template <typename T> Modular<T> operator+(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+ 
+template <typename T> Modular<T> operator-(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+ 
+template <typename T> Modular<T> operator*(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+ 
+template <typename T> Modular<T> operator/(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+ 
+template<typename T, typename U>
+Modular<T> power(const Modular<T>& a, const U& b) {
+  assert(b >= 0);
+  Modular<T> x = a, res = 1;
+  U p = b;
+  while (p > 0) {
+    if (p & 1) res *= x;
+    x *= x;
+    p >>= 1;
+  }
+  return res;
+}
+ 
+template <typename T>
+bool IsZero(const Modular<T>& number) {
+  return number() == 0;
+}
+ 
+template <typename T>
+string to_string(const Modular<T>& number) {
+  return to_string(number());
+}
+ 
+// U == std::ostream? but done this way because of fastoutput
+template <typename U, typename T>
+U& operator<<(U& stream, const Modular<T>& number) {
+  return stream << number();
+}
+ 
+// U == std::istream? but done this way because of fastinput
+template <typename U, typename T>
+U& operator>>(U& stream, Modular<T>& number) {
+  typename common_type<typename Modular<T>::Type, long long>::type x;
+  stream >> x;
+  number.value = Modular<T>::normalize(x);
+  return stream;
+}
+
+constexpr int md = (int) 1e9 + 7;
+using Mint = Modular<std::integral_constant<decay<decltype(md)>::type, md>>;
